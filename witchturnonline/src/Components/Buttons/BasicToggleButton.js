ea4fd359a75b3351ce6472050e63005a7018d01f
Tammas Hicks <tammashicks@gmail.com>
2022-12-26 08:44:02 -0800

    updating customized button component

--- witchturnonline/src/Components/Buttons/BasicToggleButton.js
+++ witchturnonline/src/Components/Buttons/BasicToggleButton.js
@@ -1,9 +1,24 @@
 import styled from "styled-components";
 
 function BasicToggleButton(props){
-    return <BasicToggle onClick={() => props.setVariable(!variable)}></BasicToggle>
+    return (
+    <BasicToggle 
+        onClick={() => {
+            props.setVariable(!props.variable)}}
+        backgroundColor={props.backgroundColor}
+        width={props.width}
+        height={props.height}
+        border={props.border}
+        >
+            {props.text}
+    </BasicToggle>);
 }
 
 export default BasicToggleButton;
 
-const BasicToggle = styled.div``;
+const BasicToggle = styled.div`
+    background-color: ${props => props.backgroundColor ? props.backgroundColor : '#d1d1d1'};
+    width: ${props => props.width? props.width : '40px'};
+    height: ${props => props.height? props.height : '40px'};
+    border: ${props => props.border? props.border : '2px solid black'};
+`;
--- witchturnonline/src/Pages/LoginLandingPage.js
+++ witchturnonline/src/Pages/LoginLandingPage.js
@@ -1,7 +1,12 @@
+import { useState } from "react";
+import BasicToggleButton from "../Components/Buttons/BasicToggleButton";
 import { DefaultPageBody } from "../Components/StyledComponents/MainStyledComponents";
 
 function LoginLandingPage () {
+    const [temp, setTemp] = useState(false);
+
     return <DefaultPageBody>
+        <BasicToggleButton variable={temp} setVariable={setTemp} backgroundColor={"White"}></BasicToggleButton> 
     </DefaultPageBody>
 }
 
