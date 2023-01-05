75332aa219b3180a6290fb8aea8796c33fdfd5c7
Tammas Hicks <tammashicks@gmail.com>
2023-01-04 22:39:59 -0800

    taking the advice of a discord friend on port connection

--- server/server.js
+++ server/server.js
@@ -16,42 +16,41 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.SERVER_ORIGIN,
+    origin: ["http://localhost:3000/", "https://witch-turn-l95l.onrender.com/"],
     methods: ["GET", "POST"],
     credentials: true,
+    withCredentials: true,
   },
 });
 
-server.listen(process.env.SERVER_PORT, () => {
-  console.log(process.env.SERVER_ORIGIN);
-  console.log(process.env.SERVER_PORT);
-  console.log("SERVER IS RUNNING");
+io.on("connection", (socket) => {
+  console.log(`user connected: ${socket.id}`);
 
-  io.on("connection", (socket) => {
-    console.log(`user connected: ${socket.id}`);
-
-    socket.on("send_message", (data) => {
-      console.log("I am sending data " + data);
-      socket.to(data.room).emit("receive_message", { message: data.message });
-    });
-
-    socket.on("join_room", (data) => {
-      console.log(data.room);
-      socket.join(data.room);
-    });
-
-    socket.on("leave_room", (data) => {
-      console.log("Leaving room: " + data.room);
-      socket.leave(data.room);
-    });
-
-    socket.off("send_message", (data) => {
-      console.log("off " + data);
-    });
-
-    socket.on("disconnect", (data) => {
-      console.log("disconnecting " + data);
-      socket.removeAllListeners();
-    });
+  socket.on("send_message", (data) => {
+    console.log("I am sending data " + data);
+    socket.to(data.room).emit("receive_message", { message: data.message });
   });
+
+  socket.on("join_room", (data) => {
+    console.log(data.room);
+    socket.join(data.room);
+  });
+
+  socket.on("leave_room", (data) => {
+    console.log("Leaving room: " + data.room);
+    socket.leave(data.room);
+  });
+
+  socket.off("send_message", (data) => {
+    console.log("off " + data);
+  });
+
+  socket.on("disconnect", (data) => {
+    console.log("disconnecting " + data);
+    socket.removeAllListeners();
+  });
+});
+
+server.listen(process.env.PORT, () => {
+  console.log("SERVER IS RUNNING");
 });
