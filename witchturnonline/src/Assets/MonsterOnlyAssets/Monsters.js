16e40acbbe80248c58c326035fcbb3cc6845a60d
Tammas Hicks <tammashicks@gmail.com>
2023-02-20 17:36:15 -0800

    adding hag to premade monsters

--- witchturnonline/src/Assets/MonsterOnlyAssets/Monsters.js
+++ witchturnonline/src/Assets/MonsterOnlyAssets/Monsters.js
@@ -2,12 +2,14 @@ import Goblin from "./Goblin.png";
 import Kobold from "./Kobold.png";
 import Bugbear from "./Bugbear.png";
 import Orc from "./Orc.png";
+import GreenHag from "./GreenHag.png";
 
 const monsters = [
   { src: Goblin, bonus: 2, armorClass: 15, name: "Goblin" },
   { src: Kobold, bonus: 2, armorClass: 12, name: "Kobold" },
   { src: Bugbear, bonus: 2, armorClass: 16, name: "Bugbear" },
   { src: Orc, bonus: 1, armorClass: 13, name: "Orc" },
+  { src: GreenHag, bonus: 1, armorClass: 16, name: "Green Hag" },
 ];
 
 export default monsters;
